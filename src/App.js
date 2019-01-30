aa166d90c0ee60c5d7f2a4761134f00b70fd66e8
Shlomi Oliel <shlomi.oliel@reedonline.co.uk>
2019-01-30 17:27:51 +0000

    adding responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+const json  = [
+  {
+    name:"title",
+    text:"sssss"
+  },
+  {
+    name:"title",
+    text:"sssss"
+  },
+  {
+    name:"title",
+    text:"sssss"
+  },
+  {
+    name:"title",
+    text:"sssss"
+  },
+  {
+    name:"title",
+    text:"sssss"
+  },
+  {
+    name:"title",
+    text:"sssss"
+  }
+
+]
+
+const Item = (props) =>{
+  return (
+    <div className="grid-item" >
+      <div className="card" >
+      <div className="card-img" ><img /></div>
+        
+        <h2>let pictures speak</h2>
+        <div className="best-seller">Best seller</div>
+        <p>Let your pictures speak for themselves.</p>
+        <div>from <span>£10.00</span></div>
+        <button >Shop Now </button>
+      </div>
+    </div>
+  )
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="products">
+         { 
+           json.map((item,i)=>{
+              return (<Item {...item}/>)
+            })
+          }
+        </div>
+     
       </div>
     );
   }
